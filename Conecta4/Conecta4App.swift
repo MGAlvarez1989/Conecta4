0e1ec2f80685b180b5915e1ee254360c8ed57700
MatiasGonzaloAlvarez <alvarezmg1989@gmail.com>
2025-01-02 09:00:02 +0100

    Initial Commit

---
+++ Conecta4/Conecta4App.swift
@@ -0,0 +1,17 @@
+//
+//  Conecta4App.swift
+//  Conecta4
+//
+//  Created by Matias Alvarez on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Conecta4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
